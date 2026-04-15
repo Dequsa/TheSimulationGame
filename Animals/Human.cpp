d0d3af9c35db03f863751b85ce5dc8481e4bdba9
dequsa <marciniak.kacperek@gmail.com>
2026-04-15 18:55:50 +0200

    work save

---
+++ Animals/Human.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marci on 4/12/2026.
+//
+
+#include "Human.h"
---
+++ Animals/Human.h
@@ -0,0 +1,17 @@
+//
+// Created by marci on 4/12/2026.
+//
+
+#ifndef C__1_HUMAN_H
+#define C__1_HUMAN_H
+#include "../Animals.h"
+
+class Human : public Animal {
+
+public:
+    Human();
+    ~Human();
+};
+
+
+#endif //C__1_HUMAN_H
